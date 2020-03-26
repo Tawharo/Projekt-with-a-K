ca4413195f6bf0ccbf102e6576024472aa362b55
Anthony Osiol <funnynoun@gmail.com>
2020-03-25 21:31:53 -0400

    update js

--- script.js
+++ script.js
@@ -34,41 +34,44 @@
 
 
     // API key for Zomato
-var authKey = "94b9c61a4d536d30ce9af305b01251f1";
-
-function buildZomatoURL() {
-    var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=italian&start=richmond";
-
-}
+var apiKey = "94b9c61a4d536d30ce9af305b01251f1";
+var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=italian&start=richmond";
 
 
 
-// var createDisplay = function(data) {
-//     var restDiv = $('<div>');
-//     // queries
-//     var restName = $('<h1>').text(data.restaurants[1].restaurant.name);
-//     console.log(data.restaurants[1].restaurant.name);
-    // if ()
-    // var restAdd = $('<h3>').text(restaurant[1].restaurant.location.address);
-    // var restRating = $('<h3>').text(restaurant[1].restaurant.user_rating);
 
+
+var createDisplay = function(data) {
+    var restDiv = $('<div>');
+    // queries
+    var restName = $('<h1>').text(data.restaurants[1].restaurant.name);
+    console.log(data.restaurants[3].restaurant.name);
+    
+    var restAdd = $('<h3>').text(restaurant[1].restaurant.location.address);
+    console.log(data.restaurants[1].restaurant.location.address);
+
+    var restRating = $('<h3>').text(restaurant[1].restaurant.user_rating);
+    console.log(data.restaurants[1].restaurant.user_rating);
+    
     // Append to newly created div
-    // restDiv.append(restName, restAdd, restRating);
+    restDiv.append(restName, restAdd, restRating);
 
     // Append to the card
-    // $('div').append(restDiv);
-// };
+    $('#results').append(restDiv);
+};
 
-function callZomato(URL) {
+
+function callZomato() {
     return $.ajax({
-        url: URL,
+        url: queryURL,
         method: "GET",
         headers: { 'user-key': apiKey }
     }).then(function (response) {
         createDisplay(response);
-        console.log(response.restaurants[2].restaurant.name);
+        // console.log(response);
     });
 };
-// callZomato();
+
+callZomato();
 
 // "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=italian&start=richmond"
